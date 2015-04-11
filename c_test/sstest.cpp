dd9d139c8abcd0635f25722c99bd32d2d57f37d0
CNLNS <1032601962@qq.com>
2015-04-11 22:19:49 +0800

    对代码进行了缩进处理

--- c_test/sstest.cpp
+++ c_test/sstest.cpp
@@ -4,13 +4,14 @@
 
 double area(double r)
 {
-double pi=3.1415926;
-return pi*r*r;
+	double pi=3.1415926;
+	return pi*r*r;
 }
 int main()
 {
-double r=0;
-printf("r=");
-scanf_s("%lf",&r);
-printf("the area is:%f\n",area(r));
-return 0;}
+	double r=0;
+	printf("r=");
+	scanf_s("%lf",&r);
+	printf("the area is:%f\n",area(r));
+	return 0;
+}
