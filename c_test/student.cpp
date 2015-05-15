4d00bd14490f40bab40f698eb32de2d66f81571f
CNLNS <1032601962@qq.com>
2015-05-15 11:04:33 +0800

    对于缩进和程序中的一些错误进行了修正

--- c_test/student.cpp
+++ c_test/student.cpp
@@ -1,72 +1,69 @@
-#include<stdio.h>
-#include<string.h>
-void readData(char names[10][20], char xuehaos[10][20],int *ages ,double* scores, char tel[10][11], int n) 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student
 {
-	printf("%d\n",n);
-	for(int i = 0;i<n;i++)
+	char name[10];
+	char xuehao[10];
+	char tel[11];
+	int age;
+	float score;
+};
+
+void student_date(int n,struct Student *s)
+{
+	char mid1[100];
+	char mid2[100];
+	char mid3[100];
+	/*char mid4[100];
+	char mid5[100];*/
+	for(int i=0;i<n;i++)
 	{
 		printf("请输入第%d个学生姓名：",i+1);
-		scanf("%s",&names[i]);
+		scanf("%s",&mid1);
+		strcpy(s[i].name,mid1);
 		printf("请输入第%d个学生学号：",i+1);
-		scanf("%s",&xuehaos[i]);
+		scanf("%s",&mid2);
+		strcpy(s[i].xuehao,mid2);
 		printf("请输入第%d个学生年龄：",i+1);
-		scanf("%d",&ages[i]);
+		scanf("%d",&s[i].age);
 		printf("请输入第%d个学生分数：",i+1);
-		scanf("%lf",&scores[i]);
+		scanf("%f",&s[i].score);
 		printf("请输入第%d个学生联系电话：",i+1);
-		scanf("%s",&tel[i]);
-	}
-
-		for(int i = 0;i<n;i++)
-	{
-		printf("\n第个学生姓名：%s",names[i]);
-		//printf("请输入第%d个学生学号：",i+1);
-		//scanf("%s",&xuehaos[i]);
-		//printf("请输入第%d个学生年龄：",i+1);
-		//scanf("%s",&ages[i]);
-		//printf("请输入第%d个学生分数：",i+1);
-		//scanf("%s",&names[i]);
-		//printf("请输入第%d个学生联系电话：",i+1);
-		//scanf("%s",&tel[i]);
-	}
-
-}
-
-void writeFile(FILE* fp, char names[10][20], char xuehaos[10][20],int *ages ,double* scores, char tel[10][11], int n) 
-{	
-	printf("cccccccccccccccccccc\n");
-	for(int i = 0;i<n;i++)
-	{
-		printf("\n%s %s\t%d\t%.1lf\t%s\n",names[i], xuehaos[i],ages[i],scores[i],tel[i]);
-		fprintf(fp, "%s %s\t%d\t%.1lf\t%s\n",names[i], xuehaos[i],ages[i],scores[i],tel[i]); 
-		//fprintf(fp, "%d %lf",333, 89.8); 
+		scanf("%s",&mid3);
+		strcpy(s[i].tel,mid3);
 	}
 }
+
+void write_file(FILE*fp,int n,struct Student *s)
+{
+	fprintf(fp,"姓名\t学号\t年龄\t分数\t电话\n");
+	for(int i=0;i<n;i++)
+	{
+	fprintf(fp,"%s\t%s\t%d\t%.1f\t%s\n",s[i].name,s[i].xuehao,s[i].age,s[i].score,s[i].tel);
+	}
+}
+
 void help()
 {
 	printf("本程序用来统计学生数据\n");
 	printf("首先请输入学生的个数\n");
-	printf("然后分别输入学生的姓名，学号，手机号，QQ号和成绩\n");
+	printf("然后分别输入学生的姓名，学号，年龄，成绩和联系电话\n");
 	printf("最终本程序会将这些数据统计后放到文件内并且输出\n\n\n");
+	printf("如果需要查看请到程序的根目录查看文件\n");
 }
 
 int main()
 {
-	int xxx[2][2]={{1,2}, {2,4}};
-
-	//姓名，学号，年龄，分数，联系电话
 	FILE*fp;
 	fp = fopen("student.txt","w+");
 	help();
 	int n;
 	printf("请输入你想统计的学生个数：");
 	scanf("%d",&n);
-	char names[10][20]={'\0'},xuehaos[10][20]={'\0'};
-	int ages[10]={0};
-	char tel[10][11]={'\0'};
-	double scores[10]={0};
-	readData(names,xuehaos,ages,scores,tel,n);
-	writeFile(fp, names,xuehaos,ages,scores,tel,n);
+	Student s[100];
+	student_date(n,s);
+	write_file(fp,n,s);
 	fclose(fp);
-
 }
